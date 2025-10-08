3c76c75b7a196990b3e6ec02509d0a4cfe4a8341
Christinah Chitombi <ST10407732@vcconnect.edu.za>
2025-10-08 13:43:57 +0200

    sonartesting

--- backend/__tests__/dummy.test.js
+++ backend/__tests__/dummy.test.js
@@ -1,6 +1,12 @@
 // backend/__tests__/dummy.test.js
-describe("Dummy backend test", () => {
-  test("always passes", () => {
-    expect(true).toBe(true);
+const { add, multiply } = require('../utils/calc');
+
+describe('Dummy backend tests', () => {
+  test('add should return correct sum', () => {
+    expect(add(2, 3)).toBe(5);
+  });
+
+  test('multiply should return correct product', () => {
+    expect(multiply(2, 3)).toBe(6);
   });
 });
---
+++ backend/utils/calc.js
@@ -0,0 +1,10 @@
+// backend/utils/calc.js
+function add(a, b) {
+  return a + b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+module.exports = { add, multiply };
