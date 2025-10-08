ea4cc57db9fadcea013ab43eae13a36ec5e4acfc
Christinah Chitombi <ST10407732@vcconnect.edu.za>
2025-10-08 12:55:40 +0200

    gg

---
+++ backend/__tests__/dummy.test.js
@@ -0,0 +1,6 @@
+// backend/__tests__/dummy.test.js
+describe("Dummy backend test", () => {
+  test("always passes", () => {
+    expect(true).toBe(true);
+  });
+});
---
+++ frontend/__tests__/dummy.test.js
@@ -0,0 +1,6 @@
+// frontend/src/__tests__/dummy.test.js
+describe("Dummy frontend test", () => {
+  test("always passes", () => {
+    expect(1 + 1).toBe(2);
+  });
+});
